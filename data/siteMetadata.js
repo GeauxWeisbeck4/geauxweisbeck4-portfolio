926ce50cfc7da8d9e6ee1047df075e1a7ec81ca2
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2024-07-31 11:21:56 -0400

    Update and test GitHub Remote con

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,13 +1,13 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Andrew Weisbeck - Full Stack Developer',
+  author: 'Andrew Weisbeck',
+  headerTitle: 'GeauxWeisbeck4',
+  description: 'Find my projects, published content, notes and more on my personal website.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://geauxweisbeck4-portfolio.vercel.app',
+  siteRepo: 'https://github.com/geauxweisbeck4-portfolio.vercel.app',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   mastodon: 'https://mastodon.social/@mastodonuser',
